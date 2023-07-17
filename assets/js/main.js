f43f35082c0caaf79352d7f23b927ddd595ce45d
mfjaconis <matheusjaconis@gmail.com>
2023-07-17 19:44:29 -0300

    Change Json UrlJob

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,42 @@
 
- (async () => {
+function updateProfileInfo(profileData) {
+   const photo = document.querySelector('.photo')
+   photo.src = profileData.photo
+   photo.alt = profileData.name
 
-    const profileData = await fetchProfileData()
-    console.log(profileData)
-    
- })() 
+   const name = document.querySelector('.name')
+   name.innerText = profileData.name
+
+   const job = document.querySelector('.job')
+   job.innerText = profileData.job 
+
+   const location = document.querySelector('.location')
+   location.innerText = profileData.location
+   
+   const phone = document.querySelector('.phone')
+   phone.innerText = profileData.phone
+
+   const email = document.querySelector('.email')
+   email.innerText = profileData.email
+}
+
+function updateSoftSkills(profileData){
+  const softSkills = document.querySelector('.softSkills')  
+  softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+
+}
+
+function updateHardSkills(profileData){
+   const hardSkills = document.querySelector('.hardSkills')
+   hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li> <img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
+}
+
+
+
+(async () => {
+
+   const profileData = await fetchProfileData()
+   updateProfileInfo(profileData)
+   updateSoftSkills(profileData)
+   updateHardSkills(profileData)
+})() 
