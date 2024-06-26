1583b152ef1d7f305b92f561f2f9f8fa92adf07e
mfjaconis <matheusjaconis@gmail.com>
2024-06-26 16:54:21 -0300

    atualizando o json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
  async function fetchProfileData() {
 
-    const url = "https://raw.githubusercontent.com/mfjaconis/js-developer-portfolio/main/data/profile.json";
+   const url = "https://raw.githubusercontent.com/mfjaconis/js-developer-portfolio/main/data/profile.json";
 
     const fetching = await fetch(url)
     
