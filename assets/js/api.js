0f5cecc34c045a8abdd3df61d3a51e1ea5b4ba66
mfjaconis <matheusjaconis@gmail.com>
2024-06-27 13:19:25 -0300

    alterando o json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,8 +1,20 @@
  async function fetchProfileData() {
 
-   const url = "https://raw.githubusercontent.com/mfjaconis/js-developer-portfolio/main/data/profile.json";
+   const url = "../../data/profile.json";
 
-    const fetching = await fetch(url)
-    
-    return await fetching.json()
+   try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error('Erro ao carregar os dados');
+    }
+
+    const data = await response.json();
+
+    return data;
+} catch (error) {
+    console.error('Erro ao buscar dados do perfil:', error);
+
+    return null;
+}
  }
